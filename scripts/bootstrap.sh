e14e5735b102930d3558fafd4c8d8d872808aa16
champii <contact@champii.io>
2018-09-01 07:26:44 +0200

    Add bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./Og src/og.og > toto/og.go
+./Og src/parse.og > toto/parse.go
+./Og src/preproc.og > toto/preproc.go
+./Og src/translator/translator.og > toto/translator/translator.go
